837a3098b848a7eb769c861ac99fbdb59062a6c7
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-25 18:24:01 +0530

    Initial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',  // Scans all files in the 'src' folder with these extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
