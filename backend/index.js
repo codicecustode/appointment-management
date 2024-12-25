e563ecc1eb6be053bb4c427188a89238b53b533f
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-25 18:27:15 +0530

    Add doctor routes to Express server and update database import path

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 
-import connectDB from './src/database/db.connection.js';
+import connectDB from './database/db.connection.js';
+import { doctorRoutes } from './src/routes/doctor.route.js';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(cors({
 // Connect to MongoDB
 connectDB();
 
+app.use('/api/v1/doctor', doctorRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port http://localhost/${process.env.PORT}`);
