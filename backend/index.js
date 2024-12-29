ba5205f4e6082b62b14f41deebb8b673434c5376
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-29 22:25:56 +0530

    Add body-parser middleware and slot routes to the server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 import cors from 'cors';
+import { Doctor } from './src/models/doctor.model.js';
 
 import connectDB from './src/database/db.connection.js';
 import { doctorRoutes } from './src/routes/doctor.route.js';
-
+import { slotRouter } from './src/routes/slot.route.js';
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
   origin: '*',
   methods: 'GET,POST,PUT,DELETE',
@@ -17,6 +22,10 @@ app.use(cors({
 connectDB();
 
 app.use('/api/v1/doctor', doctorRoutes);
+app.use('/api/v1/slot', slotRouter);
+
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port http://localhost/${process.env.PORT}`);
