05305e481c985d50b918737c8057e86a63fbca21
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-30 21:42:53 +0530

    Remove unused getSlotsByDoctorId function from doctor controller

--- backend/src/controllers/doctor.controller.js
+++ backend/src/controllers/doctor.controller.js
@@ -16,34 +16,14 @@ const getAllDoctors = async (req, res) => {
   }
 }
 
-const getSlotsByDoctorId = async (req, res) => {
-  const { doctorId } = req.params;
-  try {
-    const slotOfDocotr = await Doctor.find({ _id: doctorId }, { slots: 1 });
-    if (slotOfDocotr.length === 0) {
-      return res.status(404).json({
-        message: 'Doctor slot not found',
-        slots: [],
-      });
-    } else {
-      return res.status(200).json({
-        message: 'Doctor slot fetched successfully',
-        slots: slotOfDocotr[0].slots,
-      });
-    }
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-      slots: [],
-    });
-  }
-}
+
+
+
 
 
 
 
   export {
-    getAllDoctors,
-    getSlotsByDoctorId
+    getAllDoctors
   }
 
