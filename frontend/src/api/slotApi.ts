eb69823f9b2bf76a148a6ac1a5667773f33af6c2
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-12-30 21:40:43 +0530

    Update API URLs to use Vite environment variables and enhance slot fetching functionality

--- frontend/src/api/slotApi.ts
+++ frontend/src/api/slotApi.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-
-const API_URL = `${process.env.REACT_APP_API_URL}/slot`;
+import { ISlotForm } from '../types/slot.form.type';
+const API_URL = `${import.meta.env.VITE_APP_API_URL}/slot`;
 
 export const fetchAllSlots = async (doctorId: string) => {
   try {
-    const response = await axios.get(API_URL + `${doctorId}/getAllSlots`);
+    const response = await axios.get(API_URL + `/get-all-slots-by-doctor-id/${doctorId}`);
     return response.data;
   } catch (error: unknown) {
     if (error instanceof Error) {
@@ -13,4 +13,16 @@ export const fetchAllSlots = async (doctorId: string) => {
     return { message: 'An unknown error occurred', doctors: [] };
   }
 
+}
+
+export const createSlot  = async ( slotData: ISlotForm) => {
+  try {
+    const response = await axios.post(API_URL + '/create-slot', slotData);
+    return response;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { message: error.message, slot: null };
+    }
+    return { message: 'An unknown error occurred', slot: null };
+  }
 }
